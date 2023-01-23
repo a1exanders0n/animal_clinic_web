a6cb3ec193f383019f3ef0893d178c6abfdc2e82
Max <alxndrsn.dsgn@gmail.com>
2023-01-23 10:23:18 +0200

    implemented map feature

--- js/script.js
+++ js/script.js
@@ -1,10 +1,29 @@
-let map;
+map = new OpenLayers.Map("mapdiv");
+map.addLayer(new OpenLayers.Layer.OSM());
+   
+var pois = new OpenLayers.Layer.Text( "My Points",
+                { location:"./textfile.txt",
+                  projection: map.displayProjection
+                });
+map.addLayer(pois);
+// create layer switcher widget in top right corner of map.
+var layer_switcher= new OpenLayers.Control.LayerSwitcher({});
+map.addControl(layer_switcher);
+//Set start centrepoint and zoom    
+var lonLat = new OpenLayers.LonLat( 9.5788, 48.9773 )
+      .transform(
+        new OpenLayers.Projection("EPSG:4326"),
+        map.getProjectionObject() 
+      );
+var zoom=11;
+map.setCenter (lonLat, zoom); 
 
-function initMap() {
-  map = new google.maps.Map(document.getElementById("map"), {
-    center: { lat: -34.397, lng: 150.644 },
-    zoom: 8,
-  });
-}
+window.addEventListener("click", function() {
+  document.querySelectorAll(".olPopup").forEach(popup => popup.addEventListener("click", function(event){
+    popup.style.display = "none";
+  }))
+});
 
-window.initMap = initMap;
+document.querySelectorAll(".olPopup").forEach(popup => popup.addEventListener("click", function(event){
+  event.stopPropagation();
+}))
